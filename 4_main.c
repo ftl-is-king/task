fab4e1eac613c2f5ef18a4a4b472c14ed1f93f88
ftl-is-king <715786526@qq.com>
2025-10-20 17:13:09 +0800

    Create 4_main.c

---
+++ 4_main.c
@@ -0,0 +1,19 @@
+//202510111315
+//715786526@qq.com
+//张福君
+#include <stdio.h>
+int main()
+{
+  //三角形计算
+  printf("请输入三个整数\n");
+  int x,y,z = 0;
+  scanf ("%d,%d,%d",&x,&y,&z);
+  if (x+y <= z || x+z <= y || y+z <= x ){
+    printf("不能组成三角形\n");  
+  }
+    else{
+      printf("可以组成三角形");
+  }
+   return 0;
+  //三角形计算
+}
