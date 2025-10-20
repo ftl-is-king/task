9a87ed97491986c39eefc140092e473e08c391a7
ftl-is-king <715786526@qq.com>
2025-10-20 17:24:43 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -6,13 +6,13 @@ int main()
 {
   //三角形计算
   printf("请输入三个整数\n");
-  int x,y,z = 0;
+  int x,y,z;
   scanf ("%d,%d,%d",&x,&y,&z);
   if (x+y <= z || x+z <= y || y+z <= x ){
     printf("不能组成三角形\n");  
   }
     else{
-      printf("可以组成三角形");
+      printf("可以组成三角形\n");
   }
    return 0;
   //三角形计算
