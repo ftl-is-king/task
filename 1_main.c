fa54950a49ce533d84461c0c3aa43d37c969e916
ftl-is-king <715786526@qq.com>
2025-10-20 17:06:33 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -4,6 +4,10 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello World");
-    return 0;
+  //温度转换
+  printf("请输入摄氏温度\n");
+  float sheshidu;
+  scanf("%f",&sheshidu);
+  printf("根据您输入的摄氏温度，对应的华氏温度为%.1f\n",sheshidu*9/5+32);
+  //温度转换
 }
