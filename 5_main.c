beff52fb34c50385c4044756611c081ca6d7b632
ftl-is-king <715786526@qq.com>
2025-10-28 02:10:07 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -5,7 +5,6 @@
 int main()
 {
  int shuzu[5];
- printf("请输入四个数，用空格分隔哦\n");
  scanf("%d %d %d %d",&shuzu[0],&shuzu[1],&shuzu[2],&shuzu[3]);
  shuzu[4] = shuzu[0]+shuzu[1]+shuzu[2]+shuzu[3];
  printf("%d ,%d ,%d ,%d ,%d",shuzu[0],shuzu[1],shuzu[2],shuzu[3],shuzu[4]);
