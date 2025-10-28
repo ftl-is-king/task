fe380ad64e6258053aa1b1adc2dffce16a8adb4b
ftl-is-king <715786526@qq.com>
2025-10-28 11:55:03 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -7,6 +7,6 @@ int main()
  int shuzu[5];
  scanf("%d %d %d %d",&shuzu[0],&shuzu[1],&shuzu[2],&shuzu[3]);
  shuzu[4] = shuzu[0]+shuzu[1]+shuzu[2]+shuzu[3];
- printf("%d ,%d ,%d ,%d ,%d",shuzu[0],shuzu[1],shuzu[2],shuzu[3],shuzu[4]);
+ printf("%d %d %d %d %d",shuzu[0],shuzu[1],shuzu[2],shuzu[3],shuzu[4]);
  return 0;
 }
