d3f126a59a66a219486bb6763bad0b67e7bae722
ftl-is-king <715786526@qq.com>
2025-11-04 00:31:24 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -6,11 +6,7 @@ int x(int a,int b,int c,int d,int e);
 int main()
 {
  int i;
- int y[5];
- for ( i = 0; i < 5; i++)
- {
-  scanf("%d",&y[i]);
- }
+ int y[5] = {1,2,3,4,5}
  printf("%d",x(y[0],y[1],y[2],y[3],y[4]));
  return 0;
 }
