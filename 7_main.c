ed3a965b3b1d762516453988946604648c2cb562
ftl-is-king <715786526@qq.com>
2025-11-18 18:05:34 +0800

    Update 7_main.c

--- 7_main.c
+++ 7_main.c
@@ -1,33 +1,35 @@
 //202510111315
 //715786526@qq.com
 //张福君
-#include<stdio.h>
-void UD(int x[],int a);
-
+#include <stdio.h>
 int main()
 {
  int i;
- int shuru[5];
- for ( i = 0; i < 5; i++)
+ int arr[10];
+ for ( i = 0; i < 10; i++)
  {
-  scanf("%d",&shuru[i]);
+  scanf("%d", &arr[i]);
  }
- UD(shuru,i);
+ int j =10;
+ int *x = &arr[0];
+ for ( i = 0; i < j-1 && j > 1; i++)
+ {
+  if (*(x+i) > *(x+i+1))
+  {
+    int temp = *(x+i);
+    *(x+i) = *(x+i+1);
+    *(x+i+1) = temp;
+  }
+  if (i == j-2)
+   {
+    i = -1;
+    j = j-1;
+   }
+ }
+ for ( i = 0; i < 10; i++)
+ {
+  printf("%d ", arr[i]);
+ }
  return 0;
-}
-void UD(int x[],int a)
-{
-int i;
-for ( i = 0; i < a; i++)
-{
- if(i < a-1)
- {
-  printf("%d ",x[a-1-i]);
- }
- else
- {
-  printf("%d",x[a-1-i]);
- }
-}
 }
 
