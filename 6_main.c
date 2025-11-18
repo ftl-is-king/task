bf8dbbaa2ad4a067192d6bca14ec6734bc7612c2
ftl-is-king <715786526@qq.com>
2025-11-18 17:51:12 +0800

    Update 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -1,39 +1,22 @@
 //202510111315
 //715786526@qq.com
 //张福君
-#include<stdio.h>
-int sum(int x[],int a);
-int product(int y[],int b);
-
+#include <stdio.h>
+#include <stdlib.h>
 int main()
 {
  int i;
- int shuru[5];
+ int *arr = malloc(5 * sizeof(int));
  for ( i = 0; i < 5; i++)
  {
-  scanf("%d",&shuru[i]);
+  scanf("%d", &arr[i]);
  }
- printf("%d %d",sum(shuru,i),product(shuru,i));
-}
-int sum(int x[],int a)
-{
- int i;
- int s = 0;
- for ( i = 0; i < a; i++)
+ for ( i = 0; i < 5; i++)
  {
-  s = s + x[i];
+  printf("%d ", arr[i]);
  }
- return s;
-}
-int product(int y[],int b)
-{
- int i;
- int p = 1;
- for ( i = 0; i < b; i++)
- {
-  p = p * y[i];
- }
- return p;
+ free(arr);
+ return 0;
 }
 
-
+    
