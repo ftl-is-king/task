2162e6ec9aa5f8e4d7f9a02bf9fab94b752f41e5
ftl-is-king <715786526@qq.com>
2025-11-18 16:35:44 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,48 +1,13 @@
 //202510111315
 //715786526@qq.com
 //张福君
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
- int shuzu[3][3];
- int a = -1;
- int b = 0;
- int j;
- for(j = 0;j < 9;j++)
- {
-  if(a < 2)
-  {
-    a++;
-  }
-  scanf("%d",&shuzu[a][b]);
-  if(a == 2)
-  {
-    a = -1;
-    b++;
-  }
- }
- a = -1;
- b = 0;
- for(j = 0;j < 9;j++)
- {
-  if(a < 2)
-  {
-    a++;
-  }
-  if(a == 2)
-  {
-   printf("%d",shuzu[a][b]);
-  }
-  else
-  {
-   printf("%d ",shuzu[a][b]);
-  }
-  if(a == 2)
-  {
-    printf("\n");
-    a = -1;
-    b++;
-  }
- }
- return 0;
+    int a;
+    scanf("%d",&a);
+    int* b = &a;
+    *b = *b + 10;
+    printf("%d,%d",a,*b);
+    return 0;
 }
