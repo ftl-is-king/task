e543fc6f530180831566495bf94554a8b5933305
ftl-is-king <715786526@qq.com>
2025-11-18 17:12:49 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -4,34 +4,41 @@
 //202510111315
 //715786526@qq.com
 //张福君
-#include<stdio.h>
+#include <stdio.h>
+void swap(int *a,int *b);
 int main()
 {
- int a;
- int b[10];
- int i;
- int j = 10;
- for ( i = 0; i < 10; i++)
- {
-  scanf("%d",&b[i]);
- }
- for ( i = 0; i < j-1; i++)
- {
-  if(b[i] > b[i+1])
-  {
-   a = b[i];
-   b[i] = b[i+1];
-   b[i+1] = a;
-  }
-  if (i == j-2)
-  {
-   i = -1;
-   j--;
-  }
- }
- for ( i = 0; i < 10; i++)
- {
-  printf("%d ",b[i]);
- }
- return 0;
+    // int a;
+    // scanf("%d",&a);
+    // int* b = &a;
+    // *b = *b + 10;
+    // printf("%d,%d",a,*b);
+    // return 0;----------------------------作业1
+    // int arr[5];
+    // int i;
+    // for (i = 0; i < 5; i++)
+    // {
+    //         scanf("%d", &arr[i]);
+    // }
+    // int(*p)[5] = &arr;
+    // for ( i = 0; i < 5; i++)
+    // {
+    //    arr[i] = (*p)[i]*2;
+    // }
+    // for ( i = 0; i < 5; i++)
+    // {
+    //   printf("%d ", arr[i]);
+    // }
+    // return 0;--------------------------作业2
+  int x = 3;
+  int y = 5;
+  swap(&x,&y);
+  return 0;
+}
+void swap(int *a,int *b)
+{
+ int c = *a;
+ *a = *b;
+ *b = c;
+ printf("%d,%d",*a,*b);
 }
