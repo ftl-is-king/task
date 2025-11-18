c72d1e4a135396f3068e469af4dc296a1a1a2909
ftl-is-king <715786526@qq.com>
2025-11-18 17:36:39 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -1,16 +1,32 @@
 //202510111315
 //715786526@qq.com
 //张福君
-#include<stdio.h>
-int x(int a,int b,int c);
+#include <stdio.h>
+void plus(int *a,int b);
 int main()
 {
- printf("%d",x(1,100,1));
+ int i;
+ int arr[5];
+ for ( i = 0; i < 5; i++)
+ {
+  scanf("%d", &arr[i]);
+ }
+ int x = sizeof(arr)/sizeof(arr[0]);
+ plus(&arr[0],x);
+ for ( i = 0; i < 5; i++)
+ {
+  printf("%d ", arr[i]);
+ }
  return 0;
 }
-int x(int a,int b,int c)
-{
- int m = 0;
- m = (a+b)*((b+1-a)/c)/2;
- return m;
+
+void plus(int *a,int b)
+{   
+    int i;
+    for ( i = 0; i < b-1; i++)
+    {
+    *(a+(b-i)) = *(a+(b-i-1));
+    }  
+    *a = 0;
 }
+    
